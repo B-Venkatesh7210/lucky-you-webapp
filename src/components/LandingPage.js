af5ed50a0b8188d3d500e818fb563a025ea82598
B-Venkatesh7210 <67518179+B-Venkatesh7210@users.noreply.github.com>
2022-03-14 00:28:49 +0530

    Giveaway creation

---
+++ src/components/LandingPage.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Logo from "../img/LuckyYou Logo.png";
+
+const Home = () => {
+  return (
+    <div className="mainBg">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "2rem 0rem",
+        }}
+      >
+        <img alt="Lucky You Logo" src={Logo} style={{ width: "36%" }} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            height: "25vh",
+            width: "50%",
+          }}
+        >
+          <span className="whiteText" style={{fontSize: "28px"}}>The only platform where you can</span>
+          <span className="yellowText" style={{fontSize: "60px"}}>Create and Participate</span>
+          <span className="whiteText" style={{fontSize: "28px"}}>in giveaways of Ethereum coins.</span>
+        </div>
+        <button className="button" style={{height: "10vh", width: "20%", marginTop: "1rem"}}>
+        <span className="buttonText" style={{fontSize: "28px"}}>Enter the Dapp</span>
+
+        </button>
+      </div>
+    </div>
+  );
+};
+
+function LandingPage() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./App";
+import { MoralisProvider } from "react-moralis";
 
+const serverUrl = "https://cnmzkmj0puwx.usemoralis.com:2053/server";
+const appId = "jfYBAv1hRltfFcLJkMlDlsPf7p2Ls88pQ50JnFQw";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider serverUrl={serverUrl} appId={appId}>
+      <App />
+    </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-

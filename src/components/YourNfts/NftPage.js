2ad16c5f8551eded09c16775a9a17612be4df142
Rohit Duhan <rohitduhan2301@gmail.com>
2022-03-27 00:27:17 +0530

    sleepy mind

--- src/components/YourNfts/NftPage.js
+++ src/components/YourNfts/NftPage.js
@@ -1,11 +1,112 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../Navbar";
 import SingleNft from "./SingleNft";
+import db from "../../firebaseInit";
+import firebase from "firebase";
+import Web3 from "web3";
+import { useMoralis } from "react-moralis";
+import html2canvas from "html2canvas";
+import nftABI from "./nftAbi.json";
+import config from "../config/config.js";
+import { NFTStorage, File } from "nft.storage";
+
+const client = new NFTStorage({
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGFDQmEwMDVCYTM5YTVhMzEwQzVCNDk5RTQ1NWNGNkY4QzJmMjA0YjAiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0ODA5MjU4NDY5MCwibmFtZSI6Ikx1Y2t5WW91In0.Fl1UK3_0oJl0N1q8EFLS4mf-dPbtw8bresyBoVyeJwo",
+});
 
 const NftPage = () => {
-  let location = useLocation();
   const navigate = useNavigate();
+
+  const { account } = useMoralis();
+  const printRef = React.useRef();
+  const [typeOfGiveaway, setTypeOfGiveaway] = useState();
+  let location = useLocation();
+
+  useEffect(() => {
+    setTypeOfGiveaway(location.state.typeOfGiveaway);
+  }, []);
+
+  const handleDownloadImage = async () => {
+    const element = printRef.current;
+    console.log("here");
+    const canvas = await html2canvas(element);
+
+    let web3 = new Web3(window.web3.currentProvider);
+    const contract = new web3.eth.Contract(nftABI, config.nftContractAddress);
+
+    canvas.toBlob(async (blob) => {
+      let file = new File([blob], "LuckyYouNft.png", { type: "image/png" });
+      const metadata = await client.store({
+        name: "Lucky You NFT",
+        description: typeOfGiveaway.message,
+        image: file,
+        amount: typeOfGiveaway.amount,
+        winner: typeOfGiveaway.winner,
+        creator: typeOfGiveaway.creator,
+        particpants: typeOfGiveaway.participants,
+        deadline: typeOfGiveaway.deadline,
+        timestamp: typeOfGiveaway.timestamp,
+        isLive: typeOfGiveaway.isLive,
+        uniqueId: typeOfGiveaway.uniqueId,
+      });
+
+      console.log("metadata:", metadata.ipnft);
+      try {
+        const createCall = contract.methods
+          .createToken(metadata.url, typeOfGiveaway.uniqueId)
+          .send({
+            from: account,
+          });
+
+        createCall.on("transactionHash", (hash) => {
+          console.log(hash);
+        });
+
+        createCall.on("receipt", (recipt) => {
+          console.log("reciept:", recipt);
+          let tokenId = parseInt(recipt.events.Transfer.returnValues.tokenId);
+          console.log("tokenId:", tokenId);
+          firebase
+            .storage()
+            .ref(`nfts/${metadata.ipnft}`)
+            .put(blob)
+            .then((image) => {
+              image.ref.getDownloadURL().then((url) => {
+                console.log(url);
+                db.collection("nfts")
+                  .doc(typeOfGiveaway.uniqueId)
+                  .set({
+                    description: typeOfGiveaway.message,
+                    image: file,
+                    amount: typeOfGiveaway.amount,
+                    winner: typeOfGiveaway.winner,
+                    creator: typeOfGiveaway.creator,
+                    particpants: typeOfGiveaway.participants,
+                    deadline: typeOfGiveaway.deadline,
+                    timestamp: typeOfGiveaway.timestamp,
+                    isLive: typeOfGiveaway.isLive,
+                    uniqueId: typeOfGiveaway.uniqueId,
+                    url: url,
+                  })
+                  .then(() => {
+                    alert("Your position's NFT is minted successfully.");
+                    // navigate
+                  });
+              });
+            });
+        });
+
+        createCall.on("error", (error, recipt) => {
+          console.log(error);
+          alert(error);
+        });
+      } catch (error) {
+        alert(error.message);
+      }
+    });
+  };
 
   return (
     <div className="mainBg2">
@@ -28,7 +129,9 @@ const NftPage = () => {
             alignItems: "center",
           }}
         >
-          <SingleNft typeOfGiveaway={location.state} />
+          <div ref={printRef}>
+            <SingleNft typeOfGiveaway={location.state} />
+          </div>
           <button
             className="greenButton"
             style={{ width: "17rem", height: "5rem" }}
@@ -42,6 +145,7 @@ const NftPage = () => {
                 fontFamily: "Hand Drawn Shapes",
                 fontSize: "32px",
               }}
+              onClick={handleDownloadImage}
             >
               Mint Your NFT
             </span>
--- src/components/YourNfts/firebaseNft.js
+++ src/components/YourNfts/firebaseNft.js
@@ -1,107 +1,107 @@
-import db from "../../firebaseInit";
-import firebase from "firebase";
-import { useMoralis } from "react-moralis";
-import html2canvas from "html2canvas";
-import React, { useEffect, useState } from "react";
-import nftABI from "./nftAbi.json";
-import config from "../config/config.js";
-import { NFTStorage, File } from "nft.storage";
-import { useLocation } from "react-router-dom";
-import NftPage from "./NftPage.js";
+// import db from "../../firebaseInit";
+// import firebase from "firebase";
+// import { useMoralis } from "react-moralis";
+// import html2canvas from "html2canvas";
+// import React, { useEffect, useState } from "react";
+// import nftABI from "./nftAbi.json";
+// import config from "../config/config.js";
+// import { NFTStorage, File } from "nft.storage";
+// import { useLocation } from "react-router-dom";
+// import NftPage from "./NftPage.js";
 
-const client = new NFTStorage({
-  token:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGFDQmEwMDVCYTM5YTVhMzEwQzVCNDk5RTQ1NWNGNkY4QzJmMjA0YjAiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0ODA5MjU4NDY5MCwibmFtZSI6Ikx1Y2t5WW91In0.Fl1UK3_0oJl0N1q8EFLS4mf-dPbtw8bresyBoVyeJwo",
-});
+// const client = new NFTStorage({
+//   token:
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGFDQmEwMDVCYTM5YTVhMzEwQzVCNDk5RTQ1NWNGNkY4QzJmMjA0YjAiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0ODA5MjU4NDY5MCwibmFtZSI6Ikx1Y2t5WW91In0.Fl1UK3_0oJl0N1q8EFLS4mf-dPbtw8bresyBoVyeJwo",
+// });
 
-const NftMinting = () => {
-  const { account, web3 } = useMoralis();
-  const printRef = React.useRef();
-  const [typeOfGiveaway, setTypeOfGiveaway] = useState();
-  let location = useLocation();
+// const NftMinting = () => {
+//   const { account, web3 } = useMoralis();
+//   const printRef = React.useRef();
+//   const [typeOfGiveaway, setTypeOfGiveaway] = useState();
+//   let location = useLocation();
 
-  useEffect(() => {
-    setTypeOfGiveaway(location.state.typeOfGiveaway);
-  }, []);
+//   useEffect(() => {
+//     setTypeOfGiveaway(location.state.typeOfGiveaway);
+//   }, []);
 
-  const handleDownloadImage = async () => {
-    const element = printRef.current;
-    const canvas = await html2canvas(element);
-    const contract = new web3.eth.Contract(nftABI, config.nftContractAddress);
+//   const handleDownloadImage = async () => {
+//     const element = printRef.current;
+//     const canvas = await html2canvas(element);
+//     const contract = new web3.eth.Contract(nftABI, config.nftContractAddress);
 
-    canvas.toBlob(async (blob) => {
-      let file = new File([blob], "LuckyYouNft.png", { type: "image/png" });
-      const metadata = await client.store({
-        name: "Lucky You NFT",
-        description: typeOfGiveaway.message,
-        image: file,
-        amount: typeOfGiveaway.amount,
-        winner: typeOfGiveaway.winner,
-        creator: typeOfGiveaway.creator,
-        particpants: typeOfGiveaway.participants,
-        deadline: typeOfGiveaway.deadline,
-        timestamp: typeOfGiveaway.timestamp,
-        isLive: typeOfGiveaway.isLive,
-        uniqueId: typeOfGiveaway.uniqueId,
-      });
+//     canvas.toBlob(async (blob) => {
+//       let file = new File([blob], "LuckyYouNft.png", { type: "image/png" });
+//       const metadata = await client.store({
+//         name: "Lucky You NFT",
+//         description: typeOfGiveaway.message,
+//         image: file,
+//         amount: typeOfGiveaway.amount,
+//         winner: typeOfGiveaway.winner,
+//         creator: typeOfGiveaway.creator,
+//         particpants: typeOfGiveaway.participants,
+//         deadline: typeOfGiveaway.deadline,
+//         timestamp: typeOfGiveaway.timestamp,
+//         isLive: typeOfGiveaway.isLive,
+//         uniqueId: typeOfGiveaway.uniqueId,
+//       });
 
-      console.log("metadata:", metadata.ipnft);
-      try {
-        const createCall = contract.methods
-          .createToken(metadata.url, typeOfGiveaway.uniqueId)
-          .send({
-            from: account,
-          });
+//       console.log("metadata:", metadata.ipnft);
+//       try {
+//         const createCall = contract.methods
+//           .createToken(metadata.url, typeOfGiveaway.uniqueId)
+//           .send({
+//             from: account,
+//           });
 
-        createCall.on("transactionHash", (hash) => {
-          console.log(hash);
-        });
+//         createCall.on("transactionHash", (hash) => {
+//           console.log(hash);
+//         });
 
-        createCall.on("receipt", (recipt) => {
-          console.log("reciept:", recipt);
-          let tokenId = parseInt(recipt.events.Transfer.returnValues.tokenId);
-          console.log("tokenId:", tokenId);
-          firebase
-            .storage()
-            .ref(`nfts/${metadata.ipnft}`)
-            .put(blob)
-            .then((image) => {
-              image.ref.getDownloadURL().then((url) => {
-                console.log(url);
-                db.collection("nfts")
-                  .doc(typeOfGiveaway.uniqueId)
-                  .set({
-                    name: "Lucky You NFT",
-                    description: typeOfGiveaway.message,
-                    image: file,
-                    amount: typeOfGiveaway.amount,
-                    winner: typeOfGiveaway.winner,
-                    creator: typeOfGiveaway.creator,
-                    particpants: typeOfGiveaway.participants,
-                    deadline: typeOfGiveaway.deadline,
-                    timestamp: typeOfGiveaway.timestamp,
-                    isLive: typeOfGiveaway.isLive,
-                    uniqueId: typeOfGiveaway.uniqueId,
-                    url: url,
-                  })
-                  .then(() => {
-                    alert("Your position's NFT is minted successfully.");
-                  });
-              });
-            });
-        });
+//         createCall.on("receipt", (recipt) => {
+//           console.log("reciept:", recipt);
+//           let tokenId = parseInt(recipt.events.Transfer.returnValues.tokenId);
+//           console.log("tokenId:", tokenId);
+//           firebase
+//             .storage()
+//             .ref(`nfts/${metadata.ipnft}`)
+//             .put(blob)
+//             .then((image) => {
+//               image.ref.getDownloadURL().then((url) => {
+//                 console.log(url);
+//                 db.collection("nfts")
+//                   .doc(typeOfGiveaway.uniqueId)
+//                   .set({
+//                     description: typeOfGiveaway.message,
+//                     image: file,
+//                     amount: typeOfGiveaway.amount,
+//                     winner: typeOfGiveaway.winner,
+//                     creator: typeOfGiveaway.creator,
+//                     particpants: typeOfGiveaway.participants,
+//                     deadline: typeOfGiveaway.deadline,
+//                     timestamp: typeOfGiveaway.timestamp,
+//                     isLive: typeOfGiveaway.isLive,
+//                     uniqueId: typeOfGiveaway.uniqueId,
+//                     url: url,
+//                   })
+//                   .then(() => {
+//                     alert("Your position's NFT is minted successfully.");
+//                     // navigate 
+//                   });
+//               });
+//             });
+//         });
 
-        createCall.on("error", (error, recipt) => {
-          console.log(error);
-          alert(error);
-        });
-      } catch (error) {
-        alert(error.message);
-      }
-    });
-  };
+//         createCall.on("error", (error, recipt) => {
+//           console.log(error);
+//           alert(error);
+//         });
+//       } catch (error) {
+//         alert(error.message);
+//       }
+//     });
+//   };
 
-  <NftPage typeOfGiveaway={typeOfGiveaway} />;
-};
+//   <NftPage typeOfGiveaway={typeOfGiveaway} />;
+// };
 
-export default NftMinting;
+// export default NftMinting;

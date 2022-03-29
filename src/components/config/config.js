c51aa654b58f3d0b29fa680dcc7d327730972031
B-Venkatesh7210 <67518179+B-Venkatesh7210@users.noreply.github.com>
2022-03-30 00:37:59 +0530

    new contract and abi

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-  contractAddress: "0xf0cbF923DF288B1F236599db88C319cB7E149C5D",
-  nftContractAddress: "0x9ceA6009266EFAE0E8b81D5E1e528a77F410eD8f",
+  contractAddress: "0x489390911033F8edC52f3c1f2A23Ec628ea524F5",
+  nftContractAddress: "0xd9145CCE52D386f254917e481eB44e9943F39138",
 };
 
 export default config;

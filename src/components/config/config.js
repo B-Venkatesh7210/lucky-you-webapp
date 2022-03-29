22e881eb31ed5dc18ac60a4c07393c128cbc8375
Rohit Duhan <rohitduhan2301@gmail.com>
2022-03-30 00:02:49 +0530

    restore

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   contractAddress: "0x67807eE2C8fa3D37Ec203e156d4C15E8758Cb052",
-  nftContractAddress: "0x67DE16C206BfAB6fdf72d28e5820196E120DEaDb",
+  nftContractAddress: "0x9ceA6009266EFAE0E8b81D5E1e528a77F410eD8f",
 };
 
 export default config;

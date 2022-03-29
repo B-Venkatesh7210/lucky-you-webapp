4d32ee35cf37bd9959f59bed0feb1ec58b7b40d7
B-Venkatesh7210 <67518179+B-Venkatesh7210@users.noreply.github.com>
2022-03-30 01:31:51 +0530

    Contract changes

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  contractAddress: "0x489390911033F8edC52f3c1f2A23Ec628ea524F5",
+  contractAddress: "0x89a714d7ea7A4b52ABD881cC8aEa59c0f2092C6C",
   nftContractAddress: "0xd9145CCE52D386f254917e481eB44e9943F39138",
 };
 

8cfb2ebd335b79a34893add059e76904a81ad6f1
Rohit Duhan <rohitduhan2301@gmail.com>
2022-04-02 13:48:00 +0530

    final nft contract deployed

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   contractAddress: "0x7a973b7110A69e2d47422DD5813D6ac6963C18a0", // final contract
-  nftContractAddress: "0xC56988AF942B74A6af1d8916F794FDC242cb2D2A",
+  nftContractAddress: "0x85B40ff3D24ab9f68FfC46f5DD63A454f4E2A510", // final nft contract
 };
 
 export default config;
